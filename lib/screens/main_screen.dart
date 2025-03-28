fd3278fc73c20fbad14cf810e62d6e70d09ccfb5
Enrico Cecchetti <enrico.cecchetti@kcitylabs.fr>
2025-03-28 17:43:29 +0100

    add const to optimize center

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -227,7 +227,7 @@ class _MainViewSampleState extends ConsumerState<MainViewScreen> {
                   ),
                 ),
                     )
-                  : Center(),
+                  : const Center(),
             ),
           ];
         },
